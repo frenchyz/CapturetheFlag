72f291c5ceef1823c57f20db57b80c52ba82782a
frenchyz <xac130030@utdallas.edu>
2017-04-10 18:04:51 -0500

    First upload

---
+++ src/ctf/agent/xac130030Agent.java
@@ -0,0 +1,11 @@
+package ctf.agent;
+
+import ctf.common.*;
+
+public class xac130030Agent extends Agent{
+
+	public int getMove( AgentEnvironment inEnvironment ) {
+		//... our code here ...
+		return 0;
+		}
+}
---
+++ src/readme.txt
@@ -0,0 +1,4 @@
+To compile:
+javac -cp . ctf/agent/xac130030Agent.java
+to run
+java -cp . ctf.environment.TestPlaySurface
